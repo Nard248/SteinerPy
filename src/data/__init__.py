c2f8211d080ab56b5d81bed9cc34c2dec5c9e14c
narekmeloyan <narek@wireless2020.com>
2026-01-13 00:13:24 +0400

    fix(01-02): export GraphBuilder and SpatialIndex from data package

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -2,5 +2,14 @@
 
 from .loader import RoadNetworkLoader
 from .subset_generator import SubsetGenerator, DEFAULT_SUBSETS, DEFAULT_CENTER
+from .graph_builder import GraphBuilder
+from .spatial_index import SpatialIndex
 
-__all__ = ["RoadNetworkLoader", "SubsetGenerator", "DEFAULT_SUBSETS", "DEFAULT_CENTER"]
+__all__ = [
+    "RoadNetworkLoader",
+    "SubsetGenerator",
+    "DEFAULT_SUBSETS",
+    "DEFAULT_CENTER",
+    "GraphBuilder",
+    "SpatialIndex",
+]
